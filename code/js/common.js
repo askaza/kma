0d82f1d439dc7036ff15f2fedb537f8fd44a3dd0
aska <asya.zaharova@gmail.com>
2013-02-26 02:34:01 +0400

    faq page? placeholder

--- code/js/common.js
+++ code/js/common.js
@@ -10,34 +10,8 @@ $(document).ready(function() {
 		$('.articles-list_top .block_article:nth-child(even)').addClass('block_article_even');
 		$('.articles-list_bottom .block_article:nth-child(odd)').addClass('block_article_odd');
 	}
-	
-	
-	/*
-	 * Placeholder
-	 */
-	
-	jQuery.support.placeholder = false;
-	test = document.createElement('input, textarea');
-	if('placeholder' in test) jQuery.support.placeholder = true;
-	
-	
-	if(!$.support.placeholder) { 
-		var active = document.activeElement;
-		$(':text').focus(function () {
-			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
-				$(this).val('').removeClass('hasPlaceholder');
-			}
-		}).blur(function () {
-			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
-				$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
-			}
-		});
-		$(':text').blur();
-		$(active).focus();
-		$('form:eq(0)').submit(function () {
-			$(':text.hasPlaceholder').val('');
-		});
-	}
-	
+
+	$('input, textarea').placeholder();
+
 });
 
